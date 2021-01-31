5fecb6112ddf11f343750e0edc8e14539b5781ae
Denis Melnik <58951438+frntprog@users.noreply.github.com>
2021-01-31 21:27:21 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".multiple-items").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoSpeed: 3000,
+    dots: false,
+    centerMode: true,
+    centerPadding: 0,
+  });
+});
